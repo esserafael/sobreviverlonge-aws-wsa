260e82f1866f571e4f4ea47822e6df6f3fa38a05
esserafael <esserafael@gmail.com>
2022-10-21 18:12:03 +0100

    Change text and positions

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={michael} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Our first website!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>          
+        </p>
+        <p>          
+        </p>
+        <img src={michael} className="App-logo" alt="logo" />
       </header>
     </div>
   );
